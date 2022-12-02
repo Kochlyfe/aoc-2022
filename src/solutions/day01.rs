e6ebdd1e6ff33a3ab46c870ae8aa7020e2a1ed59
Kochlyfe <joachimvkoch@gmail.com>
2022-12-02 07:46:36 +0100

    refactor day 1 solution

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,56 +1,30 @@
-use itertools::enumerate;
-
 pub fn part_one(input: &str) -> u32 {
-    let it = input.lines();
-
-    let mut max = 0;
-    let mut current = 0;
-
-    for el in it {
-        if el.is_empty() {
-            if current > max {
-                max = current;
-            }
-            current = 0;
-            continue;
-        }
-        current += el.parse::<u32>().unwrap();
-    }
-
-    max
+    input
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .map(|cal| cal.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .max()
+        .unwrap()
 }
 
 pub fn part_two(input: &str) -> u32 {
-    let it = input.lines();
-    let num_lines = input.lines().count();
+    let mut cals = input
+    .split("\n\n")   
+    .map(|elf| 
+            elf.lines()
+            .map(|cal| 
+                    cal.parse::<u32>().unwrap()
+                )
+                .sum::<u32>()
+        ).collect::<Vec<u32>>();
 
-    let mut top_three: Vec<u32> = vec![0, 0, 0];
-    let mut current: u32 = 0;
+    cals.sort();
+    cals.reverse();
 
-    for (i, el) in enumerate(it) {
-        if i == num_lines - 1 {
-            current += el.parse::<u32>().unwrap();
-            if current > top_three[2] {
-                top_three.pop();
-                top_three.push(current);
-                continue;
-            }
-        }
-
-        if el.is_empty() {
-            if current > top_three[2] {
-                top_three.pop();
-                top_three.push(current);
-                top_three.sort();
-                top_three.reverse();
-            }
-            current = 0;
-            continue;
-        }
-        current += el.parse::<u32>().unwrap();
-    }
-
-    top_three.iter().sum()
+    cals[0..3].into_iter().sum()
 }
 
 #[cfg(test)]
