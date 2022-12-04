a4e7c70f40a3db524a04e0b3126e7a3951013894
Kochlyfe <joachimvkoch@gmail.com>
2022-12-04 11:09:16 +0100

    refactor day 4

--- src/solutions/day04.rs
+++ src/solutions/day04.rs
@@ -2,29 +2,13 @@ pub fn part_one(input: &str) -> u32 {
     input
         .lines()
         .map(|line| {
-            let pairs: Vec<_> = line.split(",").collect();
-            let first: Vec<u32> = pairs
-                .get(0)
-                .unwrap()
-                .split("-")
-                .map(|str| str.parse::<u32>().unwrap())
-                .collect();
-            let second: Vec<u32> = pairs
-                .get(1)
-                .unwrap()
-                .split("-")
-                .map(|str| str.parse::<u32>().unwrap())
-                .collect();
+            let (min1, max1, min2, max2) = parse_line_to_elfs_min_max(line);
 
-            if Some(first.get(0)) >= Some(second.get(0))
-                && Some(first.get(1)) <= Some(second.get(1))
-            {
+            if min1 >= min2 && max1 <= max2 {
                 return 1;
             }
 
-            if Some(second.get(0)) >= Some(first.get(0))
-                && Some(second.get(1)) <= Some(first.get(1))
-            {
+            if min2 >= min1 && max2 <= max1 {
                 return 1;
             }
 
@@ -35,32 +19,16 @@ pub fn part_one(input: &str) -> u32 {
 }
 
 pub fn part_two(input: &str) -> u32 {
-     input
+    input
         .lines()
         .map(|line| {
-            let pairs: Vec<_> = line.split(",").collect();
-            let first: Vec<u32> = pairs
-                .get(0)
-                .unwrap()
-                .split("-")
-                .map(|str| str.parse::<u32>().unwrap())
-                .collect();
-            let second: Vec<u32> = pairs
-                .get(1)
-                .unwrap()
-                .split("-")
-                .map(|str| str.parse::<u32>().unwrap())
-                .collect();
+            let (min1, max1, min2, max2) = parse_line_to_elfs_min_max(line);
 
-            if Some(first.get(1)) >= Some(second.get(0))
-                && Some(first.get(1)) <= Some(second.get(1))
-            {
+            if max1 >= min2 && max1 <= max2 {
                 return 1;
             }
 
-            if Some(second.get(1)) >= Some(first.get(0))
-                && Some(second.get(1)) <= Some(first.get(1))
-            {
+            if max2 >= min1 && max2 <= max1 {
                 return 1;
             }
 
@@ -68,6 +36,27 @@ pub fn part_two(input: &str) -> u32 {
         })
         .into_iter()
         .sum()
+}
+
+fn parse_line_to_elfs_min_max(line: &str) -> (u32, u32, u32, u32) {
+    let pairs: Vec<Vec<u32>> = line
+        .split(",")
+        .map(|elf| {
+            elf.split("-")
+                .map(|str| str.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .collect();
+
+    let first: &Vec<u32> = pairs.get(0).unwrap();
+    let second: &Vec<u32> = pairs.get(1).unwrap();
+
+    return (
+        *first.get(0).unwrap(),
+        *first.get(1).unwrap(),
+        *second.get(0).unwrap(),
+        *second.get(1).unwrap(),
+    );
 }
 
 #[cfg(test)]
