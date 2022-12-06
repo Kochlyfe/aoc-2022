5b073d4f8ddeab852adba6749d430b8031437cfb
Kochlyfe <joachimvkoch@gmail.com>
2022-12-06 07:26:20 +0100

    solution day 5 part 1

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -1,6 +1,4 @@
 use itertools::Itertools;
-use regex::Regex;
-use std::borrow::Cow;
 //     [D]
 // [N] [C]
 // [Z] [M] [P]
@@ -11,8 +9,6 @@ use std::borrow::Cow;
 // move 2 from 2 to 1
 // move 1 from 1 to 2
 
-use std::collections::{HashMap, hash_map};
-
 pub fn part_one(input: &str) -> String {
     let split_input: Vec<&str> = input.split("\n\n").collect();
     let crates_input = split_input.get(0).unwrap();
@@ -21,24 +17,35 @@ pub fn part_one(input: &str) -> String {
     let mut stacks = create_stacks_from_input(&crates_input);
 
     for line in actions_input.lines() {
-        let NUM_REGEX: Regex = Regex::new(r"\D").unwrap();
-        let cleaned: Cow<'_, str> = NUM_REGEX.replace_all(&line, "");
+        let spl: Vec<&str> = line.split(" ").collect();
+        let cleaned: Vec<usize> = spl
+            .into_iter()
+            .filter(|x| x.chars().all(char::is_numeric))
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
 
-        for l in cleaned.lines() {
-            let m = usize::try_from(l.chars().nth(0).unwrap().to_digit(10).unwrap()).unwrap();
-            let f = usize::try_from(l.chars().nth(1).unwrap().to_digit(10).unwrap()).unwrap() - 1;
-            let t = usize::try_from(l.chars().nth(2).unwrap().to_digit(10).unwrap()).unwrap() - 1;
+        let m = *cleaned.get(0).unwrap();
+        let f = *cleaned.get(1).unwrap() - 1;
+        let t = *cleaned.get(2).unwrap() - 1;
 
-            let mut counter = 0;
-            while counter < m {
-                let item = stacks.get_mut(f).unwrap().pop().unwrap();
-                stacks.get_mut(t).unwrap().push(item);
-                counter += 1;
-            }
+        let mut counter = 0;
+        while counter < m {
+            match stacks.get_mut(f) {
+                Some(i) => match i.pop() {
+                    Some(i) => stacks.get_mut(t).unwrap().push(i),
+                    _ => {}
+                },
+                _ => {}
+            };
+            counter += 1;
         }
     }
 
-    let res = stacks.into_iter().filter(|v| v.len() > 0 ).map(|v| v.last().unwrap().to_owned()).join("");
+    let res = stacks
+        .into_iter()
+        .filter(|v| v.len() > 0)
+        .map(|v| v.last().unwrap().to_owned())
+        .join("");
 
     res
 }
@@ -52,18 +59,21 @@ fn create_stacks_from_input(input: &str) -> Vec<Vec<String>> {
     let first_line_raw = backwards.get(0).unwrap().replace(" ", "");
     let mut stacks: Vec<Vec<String>> = vec![];
 
-    for c in first_line_raw.chars() {
-        // let i = c.to_digit(10).unwrap();
-        // stacks.push(vec![c.to_string()]);
+    for _ in first_line_raw.chars() {
         stacks.push(vec![]);
     }
 
     for line in backwards[1..].into_iter() {
-        // let double_space_regex = Regex::new(r"\s{4}").unwrap();
-        let cleaned = line.replace("    ", "0").replace(" ", "").replace("[", "").replace("]", "");
-        
+        let cleaned = line
+            .replace("    ", "0")
+            .replace(" ", "")
+            .replace("[", "")
+            .replace("]", "");
+
         for (i, c) in cleaned.chars().enumerate() {
-            if c.to_string() == "0" {continue;}
+            if c.to_string() == "0" {
+                continue;
+            }
             stacks[i].push(c.to_string())
         }
     }
