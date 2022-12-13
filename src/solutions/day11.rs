3875bfcc133d97ffa563014fe4e7ae33e692cb5b
Kochlyfe <joachimvkoch@gmail.com>
2022-12-13 21:26:07 +0100

    day 11 part 2 done

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -5,66 +5,20 @@ use itertools::Itertools;
 #[derive(Debug)]
 struct Monkey {
     id: usize,
-    items: Vec<i16>,
+    items: Vec<i64>,
     operator: String,
-    operator_num: i16,
-    test: i16,
+    operator_num: i64,
+    test: i64,
     to_true: usize,
     to_false: usize,
 }
 
-pub fn part_one(input: &str) -> i32 {
-    let mut monkeys = parse_monkeys(input);
-    // kan refactors til tuple
-    let mut item_map: HashMap<usize, Vec<i16>> = HashMap::new();
-    monkeys.iter().for_each(|m| {
-        item_map.insert(m.id, m.items.to_owned());
-    });
-
-    let mut count: Vec<i32> = vec![0; monkeys.len()];
-
-    for _ in 0..20 {
-        for monkey in monkeys.iter_mut() {
-            let mapped_items = item_map
-                .get(&monkey.id)
-                .unwrap()
-                .iter()
-                .map(|item| map_item(*item, &monkey))
-                .collect_vec();
-
-            let len = mapped_items.len() as i32;
-            for (i, it) in mapped_items {
-                item_map.get_mut(&i).unwrap().push(it);
-            }
-            count[monkey.id] += len;
-            item_map.get_mut(&monkey.id).unwrap().clear();
-        }
-    }
-
-    count.sort();
-
-    count[count.len() - 2] * count[count.len() - 1]
+pub fn part_one(input: &str) -> i64 {
+    monkey_game(input, 20, Some(3))
 }
 
-fn map_item(item: i16, monkey: &Monkey) -> (usize, i16) {
-    let worry_level = match monkey.operator.as_ref() {
-        "**" => item.pow(2),
-        "*" => item * monkey.operator_num,
-        "+" => item + monkey.operator_num,
-        _ => unreachable!(),
-    };
-    let worry_level_divided = worry_level.div(3);
-
-    let (i, it) = match worry_level_divided % monkey.test == 0 {
-        true => (monkey.to_true, worry_level_divided),
-        false => (monkey.to_false, worry_level_divided),
-    };
-
-    (i, it)
-}
-
-pub fn part_two(input: &str) -> i32 {
-    10
+pub fn part_two(input: &str) -> i64 {
+    monkey_game(input, 10000, None)
 }
 
 fn parse_monkeys(input: &str) -> Vec<Monkey> {
@@ -87,7 +41,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
                 .unwrap();
             let items = lines.get(1).unwrap()[2..]
                 .iter()
-                .map(|n| n.replace(",", "").parse::<i16>().unwrap())
+                .map(|n| n.replace(",", "").parse::<i64>().unwrap())
                 .collect_vec();
             let mut operator = lines.get(2).unwrap().get(4).unwrap().to_string();
             let operator_raw = lines.get(2).unwrap().last().unwrap();
@@ -96,7 +50,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
                     operator.push_str("*");
                     2
                 }
-                i => i.parse::<i16>().unwrap(),
+                i => i.parse::<i64>().unwrap(),
             };
 
             let test = lines
@@ -104,7 +58,7 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
                 .unwrap()
                 .last()
                 .unwrap()
-                .parse::<i16>()
+                .parse::<i64>()
                 .unwrap();
             let to_true = lines
                 .get(4)
@@ -134,6 +88,61 @@ fn parse_monkeys(input: &str) -> Vec<Monkey> {
         .collect_vec()
 }
 
+fn monkey_game(input: &str, rounds: i16, worry_level: Option<i64>) -> i64 {
+    let mut monkeys = parse_monkeys(input);
+
+    let lowest_common_denominator = monkeys.iter().map(|m| m.test).product::<i64>();
+
+    let mut item_map: HashMap<usize, Vec<i64>> = HashMap::new();
+    monkeys.iter().for_each(|m| {
+        item_map.insert(m.id, m.items.to_owned());
+    });
+
+    let mut count: Vec<i64> = vec![0; monkeys.len()];
+
+    for _ in 0..rounds {
+        for monkey in monkeys.iter_mut() {
+            let mapped_items = item_map
+                .get(&monkey.id)
+                .unwrap()
+                .iter()
+                .map(|item| map_item(*item, &monkey, worry_level, lowest_common_denominator))
+                .collect_vec();
+
+            let len = mapped_items.len() as i64;
+            for (i, it) in mapped_items {
+                item_map.get_mut(&i).unwrap().push(it);
+            }
+            count[monkey.id] += len;
+            item_map.get_mut(&monkey.id).unwrap().clear();
+        }
+    }
+
+    count.sort();
+
+    count[count.len() - 2] * count[count.len() - 1]
+} 
+
+fn map_item(item: i64, monkey: &Monkey, worry: Option<i64>, lcd: i64) -> (usize, i64) {
+    let worry_level = match monkey.operator.as_ref() {
+        "**" => item.pow(2),
+        "*" => item * monkey.operator_num,
+        "+" => item + monkey.operator_num,
+        _ => unreachable!(),
+    };
+    let worry_level_divided = match worry {
+        Some(w) => worry_level.div(w),
+        None => worry_level % lcd
+    };
+    
+    let (i, it) = match worry_level_divided % monkey.test == 0 {
+        true => (monkey.to_true, worry_level_divided),
+        false => (monkey.to_false, worry_level_divided),
+    };
+
+    (i, it)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -145,10 +154,10 @@ mod tests {
         assert_eq!(part_one(&input), 10605);
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     use aoc::read_file;
-    //     let input = read_file("examples", 11);
-    //     assert_eq!(part_two(&input), 13140);
-    // }
+    #[test]
+    fn test_part_two() {
+        use aoc::read_file;
+        let input = read_file("examples", 11);
+        assert_eq!(part_two(&input), 2713310158);
+    }
 }
